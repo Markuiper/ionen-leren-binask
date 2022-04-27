1179b657d61d898813d5ed3f2acd3e2c754e793e
Markuiper <104502059+Markuiper@users.noreply.github.com>
2022-04-27 15:26:23 +0200

    ionen uit json lijst

---
+++ js/ionen.js
@@ -0,0 +1,99 @@
+
+const woorden = document.querySelector('.woorden')
+const start = document.querySelector('#start')
+const ion = document.querySelector('#ion')
+const punt = document.querySelector('.punten')
+let score = document.querySelector('.score')
+const lichtbox = document.querySelector('.lichtbox');
+// zet de score op nul punten
+let newScore = 0
+const naamJson ="./ionenlijst.json"
+
+//shuffel een array
+
+const Shuffled = arr => {
+  const newArr = arr.slice()
+  for (let i = newArr.length - 1; i > 0; i--) {
+      const rand = Math.floor(Math.random() * (i + 1));
+      [newArr[i], newArr[rand]] = [newArr[rand], newArr[i]];
+  }
+  return newArr
+};
+
+
+//maak een lijst met de naam van de ionen
+
+const lijst = async () => {
+  const gegevens = await fetch(naamJson);
+  const data = await gegevens.json();
+  let output = [];
+  data.forEach(item => {
+  output.push(item['naam'])
+   } )
+
+ return Shuffled(output) 
+}
+
+// maak buttons
+
+const buttons = async ()=>{
+let rij = await lijst()
+console.log(rij);
+    rij.forEach(el =>{
+    woorden.innerHTML += `<button id='${el}' onclick='test(${el})'>${el}</button>`
+    })
+}
+
+buttons()
+
+// select een woord uit de lijst
+const woord = async () => {
+  const gegevens = await fetch(naamJson);
+  const data = await gegevens.json();
+  let newwoord = data[Math.floor(Math.random()*data.length)]
+  ion.innerHTML = `<div class="woordNaam" ><p class="formule">${newwoord['formule']}</p></div>`
+  localStorage.setItem("naam", newwoord['naam'])
+}
+
+
+start.addEventListener('click', ()=>{
+  lichtbox.style.display = 'none' 
+  woord()
+
+})
+
+
+// //test of button click gelijk is aan gegeven woord
+
+const test = (X)=>{
+  let Y = X.innerHTML
+  console.log(Y);
+  lichtbox.style.display = 'flex';
+      
+  if(Y === localStorage.getItem("naam")){
+    newScore++
+    score.innerHTML = 'Goed punt erbij'
+  }else{
+      score.innerHTML = 'Foutje puntje eraf'
+      newScore--
+  }
+start.innerHTML = 'Volgende'
+punt.innerHTML = `${newScore} punten`
+}
+
+
+let bezoekers = document.querySelector('.counter')
+
+const counter = async() =>{
+    const  count = await fetch("../api/hitCounter.php", {
+     method: "GET"
+   });
+ 
+   const terug = await count.text();
+
+   bezoekers.innerHTML = `U bent de <b class='num'>${terug}</b>ste bezoeker`
+}
+
+counter();
+
+ion.innerHTML = '<h1>Klik op start</h1>'
